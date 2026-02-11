d9604a3155a6f131f507f2ed49bd0dbe7749337c
Dmytro_S <dima.sds0811@gmail.com>
2026-02-11 17:31:51 +0200

    wifi_connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+
+const char* ssid_name = "TP-Link_FD24";
+const char* password = "01187441";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  delay(3000);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid_name, password);
+
+  Serial.print("Connecting to WiFi ");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
 }
